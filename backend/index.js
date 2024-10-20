faa688362b503d3b55744525f8e812be3fb31d85
santosh <styadav018@gmail.com>
2024-10-21 01:25:02 +0530

    feat get salash

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,28 @@
-const express = require("express");
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const AuthRouter = require('./Routes/AuthRouter')
 const ProductsRoutes = require('./Routes/ProductRouter')
 
-require('dotenv').config();
+require('dotenv').config()
 require('./Models/db')
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000
 
-app.get("/ping",(req,res)=>{
-    res.send("PONG")
-});
+server.get('/', (req, res) => {
+  res.send('server connnected')
+})
+app.get('/ping', (req, res) => {
+  res.send('PONG')
+})
 
-app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.json())
+app.use(cors())
 
-app.use('/auth',AuthRouter);
-app.use('/products',ProductsRoutes)
+app.use('/auth', AuthRouter)
+app.use('/products', ProductsRoutes)
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on 4000`)
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`)
 })
