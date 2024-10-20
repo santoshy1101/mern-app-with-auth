871668c580e2e4849b14e727664f6ee27966cf22
santosh <styadav018@gmail.com>
2024-10-21 02:52:27 +0530

    removed to older version of index.js

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,25 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const AuthRouter = require('./Routes/AuthRouter')
 const ProductsRoutes = require('./Routes/ProductRouter')
 
-require('dotenv').config()
-
-app.use(bodyParser.json())
-app.use(
-  cors({
-    origin: '',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    credentials: true,
-  }),
-)
-
+require('dotenv').config();
 require('./Models/db')
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('server connnected!!!')
-})
-app.get('/ping', (req, res) => {
-  res.send('PONG')
-})
+app.get("/ping",(req,res)=>{
+    res.send("PONG")
+});
 
-app.use('/auth', AuthRouter)
-app.use('/products', ProductsRoutes)
+app.use(bodyParser.json());
+app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`server is running on ${PORT}`)
+app.use('/auth',AuthRouter);
+app.use('/products',ProductsRoutes)
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on 4000`)
 })
